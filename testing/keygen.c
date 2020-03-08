581b55a11e288fc54b0a33ee47037fb867aaecb7
Hamza Munaf <munafh@os1.engr.oregonstate.edu>
2020-03-08 11:32:51 -0700

    Working on it

--- testing/keygen.c
+++ testing/keygen.c
@@ -7,21 +7,21 @@ static const char sourceArray[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 
 int main (int argc, char* argv[]) {
 	int loopcounter;
-	int int_lengthVal = atoi(argv[1]);
+	int lengthCounter = atoi(argv[1]);
 	//string of key used through out file
-	char char_keyValue[int_lengthVal+1];
+	char char_keyValue[lengthCounter+1];
 	//applying time as a seed value to random generation
 	srand(time(0));
 
 	// while loop used for random value assigning to a key
 	loopcounter=0;
-	while ( loopcounter<int_lengthVal ) {
+	while ( loopcounter<lengthCounter ) {
 		int randm_value = rand() % 27;
 		char_keyValue[loopcounter] = sourceArray[randm_value];
 		loopcounter++;
 	}
 	// append end of character file
-	char_keyValue[int_lengthVal] = '\0';
+	char_keyValue[lengthCounter] = '\0';
 	printf("%s\n", char_keyValue);
 
 	return 0;
