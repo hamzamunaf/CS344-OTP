a19284216010d09b03b6bb963cc5becc86138bfa
Hamza Munaf <munafh@os1.engr.oregonstate.edu>
2020-03-09 09:47:08 -0700

    Checking the length of the key

--- testing/otp_enc.c
+++ testing/otp_enc.c
@@ -10,8 +10,8 @@
 
 
 // encryption for client
-
-
+#define MAX_CHAR 80000
+// otp_enc plaintext1 mykey 57171 > ciphertext1
 //otp_enc plaintext key port, need three arguments to make it work
 void error(const char *msg) { perror(msg); exit(0); } // Error function used for reporting issues
 
@@ -20,29 +20,41 @@ int main(int argc, char *argv[])
 	int socketFD, portNumber, charsWritten, charsRead;
 	struct sockaddr_in serverAddress;
 	struct hostent* serverHostInfo;
-	char keygen[256];
-  char enc_text[256];
+	char keygen[MAX_CHAR];
+  char enc_text[MAX_CHAR];
+	int keygenlen;
+	int enc_textlen;
 
 	if (argc < 4) { fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); exit(0); } // Check usage & args
 
 	// Set up the server address struct
   //Counter program
-  printf("Argument counter port number:  %s\n", argv[1]);
-  printf("Argument counter keyfile %s\n", argv[2]);
-  printf("Argument counter normal file %s\n", argv[3]);
+	  printf("Argument counter normal file %s\n", argv[1]);
+		printf("Argument counter keyfile %s\n", argv[2]);
+  	printf("Argument counter port number:  %s\n", argv[3]);
+
+
 
 //File checking for key and will transfer key to see how it works
+//lets check length for key and file for comparison
   FILE* file_pointer = fopen(argv[2], "r");
-  fgets(keygen, 256, file_pointer);
+  fgets(keygen, MAX_CHAR, file_pointer);
   fclose(file_pointer);
+	keygenlen=strlen(keygen);
 // File for the plain text
-  FILE* file_pointer2 = fopen(argv[3], "r");
-  fgets(enc_text, 256, file_pointer2);
+  FILE* file_pointer2 = fopen(argv[1], "r");
+  fgets(enc_text, MAX_CHAR, file_pointer2);
   fclose(file_pointer2);
+	enc_textlen=strlen(enc_text);
+
+	if (keygenlen < enc_textlen){
+		printf("Error, key is shorter than the text to encrypt\n");
+		exit(1);
+	}
 
 
 	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
-	portNumber = atoi(argv[1]); // Get the port number, convert to an integer from a string
+	portNumber = atoi(argv[3]); // Get the port number, convert to an integer from a string
 	serverAddress.sin_family = AF_INET; // Create a network-capable socket
 	serverAddress.sin_port = htons(portNumber); // Store the port number
 	serverHostInfo = gethostbyname("localhost"); // Convert the machine name into a special form of address
@@ -68,7 +80,7 @@ int main(int argc, char *argv[])
 	// Get return message from server
 	// memset(buffer2, '\0', sizeof(buffer2)); // Clear out the buffer again for reuse
 	// charsRead = recv(socketFD, buffer2, sizeof(buffer2) - 1, 0); // Read data from the socket, leaving \0 at end
-	// if (charsRead < 0) error("CLIENT: ERROR reading from socket");
+	if (charsRead < 0) error("CLIENT: ERROR reading from socket");
 	// printf("CLIENT: I received this from the server: \"%s\"\n", buffer2);
 
 	close(socketFD); // Close the socket
