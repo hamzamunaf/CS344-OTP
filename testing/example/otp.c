88571c9ee0be32ddd6c686e2b4cf46af668acdc9
Hamza Munaf <munafh@os1.engr.oregonstate.edu>
2020-03-09 12:31:12 -0700

    Will work on encryption on a seperate file to avoid any problem and see how it encrypts

---
+++ testing/example/otp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(){
+// lets work on otp here and see how it reacts
+
+}
