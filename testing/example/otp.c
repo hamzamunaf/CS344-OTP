789fa3458d24ac5c138420231adbe90c5e08f35f
Hamza Munaf <munafh@os1.engr.oregonstate.edu>
2020-03-09 12:35:51 -0700

    Environment is ready and now working on encryption

--- testing/example/otp.c
+++ testing/example/otp.c
@@ -6,7 +6,53 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int main(){
-// lets work on otp here and see how it reacts
+#define MAX_CHAR 80000
+
+int main(int argc, char *argv[]){
+  // lets work on otp here and see how it reacts
+  char keygen[MAX_CHAR];
+  char enc_text[MAX_CHAR];
+  int keygenlen;
+  int enc_textlen;
+
+  FILE* file_pointer = fopen(argv[2], "r");
+  char charactercounter;
+  int count = 0;
+  while((charactercounter = fgetc(file_pointer)) != EOF)
+  {
+    if(charactercounter > 32 && charactercounter < 65 && charactercounter < 90)
+    {
+      fprintf(stderr, "ERROR: Keygen file has invalid characters \n");
+      exit(1);
+    }
+
+    keygen[count] = charactercounter;
+    count++;
+  }
+  fclose(file_pointer);
+  keygen[strcspn(keygen, "\n")] = '\0';
+  keygenlen=strlen(keygen);
+  FILE* file_pointer2 = fopen(argv[1], "r");
+  charactercounter='\0';
+  count = 0;
+  while((charactercounter = fgetc(file_pointer2)) != EOF)
+  {
+    if(charactercounter > 32 && charactercounter < 65 && charactercounter < 90)
+    {
+      fprintf(stderr, "ERROR: Plaintext, file has invalid characters \n");
+      exit(1);
+    }
+
+    enc_text[count] = charactercounter;
+    count++;
+  }
+  fclose(file_pointer2);
+  enc_text[strcspn(enc_text, "\n")] = '\0';
+  enc_textlen=strlen(enc_text);
+
+  printf("Key value: %s\n", keygen);
+  printf("Plain text value: %s\n", enc_text);
+
+  // Lets encrypt it and see how that works out
 
 }
