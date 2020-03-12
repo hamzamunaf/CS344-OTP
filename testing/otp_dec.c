a38ab613cce4dd5b4531a00e23d98197c6ececed
Hamza Munaf <munafh@os1.engr.oregonstate.edu>
2020-03-12 10:51:55 -0700

    Working on setting up decryption client and server

---
+++ testing/otp_dec.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <assert.h>
+#include <stdbool.h>
+
+//basic BasicArray
+static const char BasicArray[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+// encryption for client
+#define MAX_CHAR 90000
+#define BASICLEN 28
+
+void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
+
+int main(int argc, char *argv[])
+{
+	int socketFD, portNumber, charsWritten, charsRead;
+	struct sockaddr_in serverAddress;
+	struct hostent* serverHostInfo;
+	char keygen[MAX_CHAR];
+  char cipher_text[MAX_CHAR];
+	int keygenlen;
+	int cipher_textlen;
+	int basicarraylen;
+	// char EncryptedMessage[MAX_CHAR];
+
+	if (argc < 4) { fprintf(stderr,"USAGE: %s hostname port\n", argv[3]); exit(0); } // Check usage & args
+
+	// Set up the server address struct
+  //Counter program
+	  // printf("Argument counter normal file %s\n", argv[1]);
+		// printf("Argument counter keyfile %s\n", argv[2]);
+  	// printf("Argument counter port number:  %s\n", argv[3]);
+
+
+
+//File checking for key and will transfer key to see how it works
+//lets check length for key and file for comparison and check all properties while reading
+  FILE* file_pointer = fopen(argv[2], "r");
+  // fgets(keygen, MAX_CHAR, file_pointer);
+	char charactercounter;
+	int count = 0;
+	while((charactercounter = fgetc(file_pointer)) != EOF)
+	{
+		if(charactercounter > 32 && charactercounter < 65 && charactercounter < 90)
+		{
+			fprintf(stderr, "ERROR: Keygen file has invalid characters \n");
+			exit(1);
+		}
+
+		keygen[count] = charactercounter;
+		count++;
+	}
+	fclose(file_pointer);
+	//need to get rid off new line and put a "\0"
+	// this didn't works
+	keygen[strcspn(keygen, "\n")] = '\0';
+	// Keygen[count]='\0';
+	keygenlen=strlen(keygen);
+	// printf(" key:  %s\n", keygen);
+// File for the plain text
+  FILE* file_pointer2 = fopen(argv[1], "r");
+  // fgets(cipher_text, MAX_CHAR, file_pointer2);
+	charactercounter='\0';
+	count = 0;
+	while((charactercounter = fgetc(file_pointer2)) != EOF)
+	{
+		if(charactercounter > 32 && charactercounter < 65 && charactercounter < 90)
+		{
+			fprintf(stderr, "ERROR: Plaintext, file has invalid characters \n");
+			exit(1);
+		}
+
+		cipher_text[count] = charactercounter;
+		count++;
+	}
+	// cipher_text[count]='\0';
+  fclose(file_pointer2);
+	cipher_text[strcspn(cipher_text, "\n")] = '\0';
+	cipher_textlen=strlen(cipher_text);
+	// printf(" Text:  %s\n", cipher_text);
+// Note that the key passed in must be at least as big as the plaintext.
+	if (keygenlen < cipher_textlen){
+		fprintf(stderr, "ERROR: Keygen is shorter than plain text\n");
+		exit(1);
+	}
+
+  memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+	portNumber = atoi(argv[3]); // Get the port number, convert to an integer from a string
+	serverAddress.sin_family = AF_INET; // Create a network-capable socket
+	serverAddress.sin_port = htons(portNumber); // Store the port number
+	serverHostInfo = gethostbyname("localhost"); // Convert the machine name into a special form of address
+	if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); }
+	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
+
+	// Set up the socket
+  // Set up the socket
+  socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+  if (socketFD < 0) error("CLIENT: ERROR opening socket");
+
+  // Connect to server
+  if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
+    error("CLIENT: ERROR connecting");
---
+++ testing/otp_dec_d.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+static const char BasicArray[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+// encryption for client
+#define MAX_CHAR 80000
+#define BASICLEN 27
+// daemon server for encryption
+// This child process of otp_enc_d must first check to make sure it is communicating with otp_enc (see otp_enc, below). After verifying that the connection to otp_enc_d is coming from otp_enc, then this child receives from otp_enc plaintext and a key via the communication socket (not the original listen socket). The otp_enc_d child will then write back the ciphertext to the otp_enc process that it is connected to via the same communication socket. Note that the key passed in must be at least as big as the plaintext.
+//There are two ways we can recive the message from the client either I get the size of mesage and recieve the message or I can break it into two different things
+// Not sure what to do :()
+void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
+
+int main(int argc, char *argv[])
+{
+	int listenSocketFD, establishedConnectionFD, portNumber, charsRead, charsWritten;
+	socklen_t sizeOfClientInfo;
+	int keygenlen=0;
+	int plain_textlen=0;
+	char buffer[10000];
+	// char keygen[MAX_CHAR];
+	struct sockaddr_in serverAddress, clientAddress;
+
+	if (argc < 2) { fprintf(stderr,"USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
+
+	// Set up the address struct for this process (the server)
+	memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
+	portNumber = atoi(argv[1]); // Get the port number, convert to an integer from a string
+	serverAddress.sin_family = AF_INET; // Create a network-capable socket
+	serverAddress.sin_port = htons(portNumber); // Store the port number
+	serverAddress.sin_addr.s_addr = INADDR_ANY; // Any address is allowed for connection to this process
+
+	// Set up the socket
+	listenSocketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
+	if (listenSocketFD < 0) error("ERROR opening socket");
+
+	// Enable the socket to begin listening
+	if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to port
+	error("ERROR on binding");
+	listen(listenSocketFD, 5); // Flip the socket on - it can now receive up to 5 connections
+
+
+	// Accept a connection, blocking if one is not available until one connects
+	sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
+	establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
+	if (establishedConnectionFD < 0) error("ERROR on accept");
